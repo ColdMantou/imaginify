b892fc1e0f34f96db050addb123bb3e554e3b6c3
ColdMantou <53591195+ColdMantou@users.noreply.github.com>
2024-05-04 07:56:17 +0800

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,19 @@
-import {
-  clerkMiddleware,
-  createRouteMatcher
-} from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  '/',
-  '/credits(.*)',
+// sign-up, sign-up and /api/webhook/clerk will be public routes
+// the rest will be protected
+const isPublicRoute = createRouteMatcher([
+  "/((?!sign-in|sign-up|api/webhook/clerk).*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isPublicRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    "/((?!.*\\..*|_next).*)", // Don't run middleware on static files
+    "/", // Run middleware on index page
+    "/(api|trpc)(.*)",
+  ], // Run middleware on API routes
 };
